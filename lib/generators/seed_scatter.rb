15fd41c80070c035a9b199c9615af107721b6dc2
Alex Padgett <apadgett@topicdesign.com>
2013-07-18 10:30:01 -0400

    Extend NamedBase generator and change source_root

---
+++ lib/generators/seed_scatter.rb
@@ -0,0 +1,11 @@
+require 'rails/generators/named_base'
+
+module SeedScatter
+  module Generators
+    class Base < Rails::Generators::NamedBase
+      def self.source_root
+        File.expand_path(File.join(File.dirname(__FILE__), generator_name, 'templates'))
+      end
+    end
+  end
+end
