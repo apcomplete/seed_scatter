45296f6c1e03ca448a27e1a65212feb3b5ce05e5
Alex Padgett <apadgett@topicdesign.com>
2013-07-18 11:44:36 -0400

    Move railtie to it's own file

--- lib/seed_scatter.rb
+++ lib/seed_scatter.rb
@@ -1,12 +1,6 @@
 require "seed_scatter/version"
+require "seed_scatter/engine"
+require "seed_scatter/railtie"
 
 module SeedScatter
-  class Railtie < ::Rails::Railtie
-
-    generators do
-      require 'rails/generators/rails/model/model_generator'
-      Rails::Generators::ModelGenerator.send(:hook_for,:seed_scatter)
-      Rails::Generators::ModelGenerator.send(:class_option, :seed_scatter, :default => 'seed_scatter')
-    end
-  end
 end
